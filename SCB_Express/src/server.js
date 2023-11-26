5bd8905aaaaae80ed4f8c38d55ad757be9a5c508
hieucv267@gmail.com <hieucv276@gmail.com>
2023-11-26 21:37:22 +0700

    NodeJs

---
+++ SCB_Express/src/server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// req/res requests/response
+app.get('/', (req, res) => {
+  res.send('Hello world! 12345');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
